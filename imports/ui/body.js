f2ef3653729eed288eb9f8b94e29b120d21fadf4
Tran Thanh Tu <gm.tranthanhtu@gmail.com>
2016-04-05 02:11:39 +0900

    Create folder imports

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,11 @@
-/**
- * Created by gm3t on 4/5/16.
- */
+import { Template } from 'meteor/templating';
+
+import './body.html';
+
+Template.body.helpers({
+    tasks: [
+        { text: 'This is task 1' },
+        { text: 'This is task 2' },
+        { text: 'This is task 3' },
+    ],
+});
